5fe893bdf67f70c6b4161b73ed4994aa9376e7b2
Markus Nordin <markus.nordin@gmail.com>
2017-05-10 13:45:55 +0200

    Configure mime type

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module PdfGen
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.use WickedPdf::Middleware
   end
 end
