989f267557c621b08b870b41439c716167f80a41
Markus Nordin <markus.nordin@gmail.com>
2017-05-11 15:37:15 +0200

    Remove layout config
    Since the controller seems to be overwriting this setting anyway

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -17,7 +17,7 @@ WickedPdf.config = {
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
-  layout: 'pdf.html',
+  # layout: 'pdf.html',
   page_size: 'A4',
   background: true,
   margin:  {
